a95860e03d8fd161a73c9e22df320cc7d8ff9a16
jam-sha <62854837+jam-sha@users.noreply.github.com>
2021-10-19 15:18:09 -0400

    Update BigOSorting.java

--- BigOSorting.java
+++ BigOSorting.java
@@ -1,24 +1,27 @@
 
 import java.util.Scanner;
 import java.util.Random;
-//test
+
 public class BigOSorting {
     public static void main (String[] args) {
         Random ran = new Random();
       
         Scanner input = new Scanner(System.in);
         int max = -1;
-        System.out.println("Selection, insrtation or merge");
+        System.out.println("Selection, insertation or merge sort?");
         String choice = input.nextLine(); // to do: put in methods
 
         choice = choice.toLowerCase();
 
-        System.out.println("How much do you want to sort?");
-
+        System.out.println("How much do you want to sort? (-1 for all sizes)");
         int size = input.nextInt();
 
+        if (size != -1) {
         int[] data = new int[size];
-
+            
+        } else {
+            // sort 5 arrays of 10000, 20000, 40000, 80000, 160000 size
+        }
         for (int i = 0; i < size; i++) {
             data[i] = ran.nextInt(Integer.MAX_VALUE);
         }
@@ -34,17 +37,10 @@ public class BigOSorting {
         System.out.println("Max: " + data[size-1]);
         System.out.println("Min: " + data[0]);
 
-
-
-
-
     }
 
-
-
     public static void selectionSort(int size, int[] arr){  
-        for (int i = 0; i < size-1; i++)  
-        {  
+        for (int i = 0; i < size-1; i++)  {  
             int index = i;  
             for (int j = i + 1; j < size; j++){  
                 if (arr[j] < arr[index]){  
@@ -56,5 +52,4 @@ public class BigOSorting {
             arr[i] = smallerNumber;  
         }  
     }
-
 }
