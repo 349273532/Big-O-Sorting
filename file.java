d5a0ce8d75f1167dbb791e6825b97d6c620be75d
349273532 <59988006+349273532@users.noreply.github.com>
2021-10-19 14:59:03 -0400

    This is the file containing the main

---
+++ file.java
@@ -0,0 +1,60 @@
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class file {
+    public static void main (String[] args) {
+        Random ran = new Random();
+      
+        Scanner input = new Scanner(System.in);
+        int max = -1;
+        System.out.println("Selection, insrtation or merge");
+        String choice = input.nextLine(); // to do: put in methods
+
+        choice = choice.toLowerCase();
+
+        System.out.println("How much do you want to sort?");
+
+        int size = input.nextInt();
+
+        int[] data = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            data[i] = ran.nextInt(Integer.MAX_VALUE);
+        }
+
+        if (choice.equals("selection")) {
+            selectionSort(size,data);
+        } else if (choice.equals("insertation")) {
+            // insertation call
+        } else if (choice.equals("merge")) {
+            // merge call
+        }
+
+        System.out.println("Max: " + data[size-1]);
+        System.out.println("Min: " + data[0]);
+
+
+
+
+
+    }
+
+
+
+    public static void selectionSort(int size, int[] arr){  
+        for (int i = 0; i < size-1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < size; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }
+
+}
