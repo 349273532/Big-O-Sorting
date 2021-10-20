45f175a9408261cc4378e1d5f3cc0d61eb868e11
jam-sha <62854837+jam-sha@users.noreply.github.com>
2021-10-20 09:49:20 -0400

    Update BigOSorting.java

--- BigOSorting.java
+++ BigOSorting.java
@@ -4,38 +4,50 @@ import java.util.Random;
 
 public class BigOSorting {
     public static void main (String[] args) {
-        Random ran = new Random();
+       
       
         Scanner input = new Scanner(System.in);
-        int max = -1;
+       
         System.out.println("Selection, insertation or merge sort?");
-        String choice = input.nextLine(); // to do: put in methods
-
+        String choice = input.nextLine();
         choice = choice.toLowerCase();
 
         System.out.println("How much do you want to sort? (-1 for all sizes)");
         int size = input.nextInt();
 
         if (size != -1) {
-        int[] data = new int[size];
-            
-        } else {
-            // sort 5 arrays of 10000, 20000, 40000, 80000, 160000 size
-        }
-        for (int i = 0; i < size; i++) {
-            data[i] = ran.nextInt(Integer.MAX_VALUE);
-        }
+        int[] data = createDataset(size);
 
-        if (choice.equals("selection")) {
+        if (choice.equals("selection")) { // make this a method
             selectionSort(size,data);
         } else if (choice.equals("insertation")) {
             // insertation call
         } else if (choice.equals("merge")) {
             // merge call
         }
-
         System.out.println("Max: " + data[size-1]);
         System.out.println("Min: " + data[0]);
+
+        } else { 
+            int[]arr1 = createDataset(10000);
+            int[]arr2 = createDataset(20000);
+            int[]arr3 = createDataset(40000);
+            int[]arr4 = createDataset(80000);
+            int[]arr5 = createDataset(160000);
+            if (choice.equals("selection")) { 
+                selectionSort(10000,arr1);
+                selectionSort(20000,arr2);
+                selectionSort(40000,arr3);
+                selectionSort(80000,arr4);
+                selectionSort(160000,arr5);
+
+            } else if (choice.equals("insertation")) {
+                // insertation call
+            } else if (choice.equals("merge")) {
+                // merge call
+            }
+        }
+        
 
     }
 
@@ -51,5 +63,19 @@ public class BigOSorting {
             arr[index] = arr[i];  
             arr[i] = smallerNumber;  
         }  
+    }
+
+    public static int[] createDataset(int size) {
+        Random ran = new Random();
+        int[] data = new int[size];
+            
+        for (int i = 0; i < size; i++) {
+            data[i] = ran.nextInt(Integer.MAX_VALUE);
+        }
+        return data;
+    }
+
+    public static void chooseSort () {
+
     }
 }
